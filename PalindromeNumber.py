8dcf27bf2c7a2ffbf971e90020b5e903a070ae28
Rudreshcg <rudresha.gangaswamaiah@softsuave.com>
2022-10-26 14:37:26 +0530

    updating

--- PalindromeNumber.py
+++ PalindromeNumber.py
@@ -1,4 +1,6 @@
 """
+2. Palindrome Number:-
+
 Given an integer x, return true if x is palindrome integer.
 
 An integer is a palindrome when it reads the same backward as forward.
--- RomantoInteger.py
+++ RomantoInteger.py
@@ -1,4 +1,6 @@
 """
+3. Roman to Integer:-
+
 Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
 
 Symbol       Value
