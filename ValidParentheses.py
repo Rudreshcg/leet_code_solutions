f9e3523e808708a6199f9132692b955b3ce3d7fe
Rudreshcg <rudresha.gangaswamaiah@softsuave.com>
2022-10-31 12:21:07 +0530

    solution for ValidParentheses.py

--- ValidParentheses.py
+++ ValidParentheses.py
@@ -22,3 +22,26 @@ Example 3:
 Input: s = "(]"
 Output: false
 """
+
+
+# def is_valid(s):
+#     if s == '':
+#         return True
+#     elif s == '()' or s == '()[]{}' or s == '{}' or s == '[]':
+#         return True
+#     else:
+#         return False
+#
+# print(is_valid("(]"))
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        pair = dict(('()', '[]', '{}'))
+        st = []
+        for x in s:
+            if x in '([{':
+                st.append(x)
+            elif len(st) == 0 or x != pair[st.pop()]:
+                return False
+        return len(st) == 0
+    print(isValid(self=None, s="{()}"))
